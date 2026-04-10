9779a38e152c0d86a204b5adfad70755978499fe
siwakornsanpnag <siwakorn.sanpang@gmail.com>
2026-04-10 09:34:22 +0700

    แก้คำผิดบน sidebar

--- app/center-for-professional/components/SidebarTabsSection/Sidebar.tsx
+++ app/center-for-professional/components/SidebarTabsSection/Sidebar.tsx
@@ -21,7 +21,7 @@ interface SidebarProps {
 export default function Sidebar({ menuItems, activeTab, onTabChange }: SidebarProps) {
   return (
     <div className={styles.historyMenu}>
-      <h3>"ยกระดับสมุนไพรไทย<br />ด้วยมาตรฐานเภสัชกรรมระดับสากล"</h3>
+      <h3>"อัปเกรดทักษะวิชาชีพ... <br />ไม่หยุดเรียนรู้ ก้าวสู่เภสัชกรแถวหน้า"</h3>
       <ul>
         {menuItems.map((item) => (
           <li
